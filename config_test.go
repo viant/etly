c63a601e242e098ba4a31fff7a75d086585266e2
rzhaovi <51424930+rzhaovi@users.noreply.github.com>
2019-08-13 14:44:31 -0700

    Escape actual Transfer parameters (#28)
    ENG-23001: Escape actual Transfer parameters

--- config_test.go
+++ config_test.go
@@ -2,6 +2,7 @@ package etly
 
 import (
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 	"time"
 )
@@ -21,4 +22,28 @@ func TestDuration_Get(t *testing.T) {
 		//Inbuilt type duration is int64 and always holds value at nano second level
 		assert.Equal(t, 240000000, int(duration))
 	}
+}
+
+func TestTransferPrintFormat(t *testing.T) {
+	const sourceStringToEscape = "<dateFormat:yyyy>/<dateFormat:MM>/<dateFormat:dd>/<dateFormat:HH>"
+	const targetStringToEscape = "<dateFormat:HH>"
+
+	transferConfig, err := NewTransferConfigFromURL("file://" + GetCurrentWorkingDir() + "/test/transfer_config3.json")
+	assert.Nil(t, err)
+	assert.NotNil(t, transferConfig)
+	assert.NotNil(t, transferConfig.Transfers[0])
+
+
+	transfer := transferConfig.Transfers[0]
+	name := transfer.Name
+	source := transfer.Source.Name
+	target := transfer.Target.Name
+
+	assert.True(t, strings.Contains(source, sourceStringToEscape))
+	assert.True(t, strings.Contains(target, targetStringToEscape))
+
+	toString := transfer.String();
+	assert.True(t, strings.Contains(toString, name))
+	assert.True(t, !strings.Contains(toString, sourceStringToEscape))
+	assert.True(t, !strings.Contains(toString, targetStringToEscape))
 }
